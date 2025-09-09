3b8be6b4c7627386264ea75bd7932be33a2ddb3b
hldsandanika <dasunisandanika1@gmail.com>
2025-09-09 15:41:20 +0530

    version 1

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def factorial(n):
-     if n ==0:
+     if n == 0:
         return 1
      else:
-        return n * factorial(n-1)
+        return n * factorial(n-2)
         
 number =5
 result = factorial(number)
